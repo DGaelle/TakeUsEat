a63f076764141ace9436ee7ad19456de543eb85c
gaelle ducrocq <ducrocqgaelle@gmail.com>
2020-02-24 16:28:58 +0100

    Ajoutez des fichiers projet.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AfpEat.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AfpEatEntities db = new AfpEatEntities();
+
+        // GET: Restaurants
+        public ActionResult Index()
+        {
+            //on recupere tous les restos de la bdd vers la page d'accueil
+            //ViewBag.Restaurants = db.Restaurants.ToList();
+            ViewBag.TypeCuisines = db.TypeCuisines.ToList();
+
+            //var typeCuisines=db.TypeCuisines.ToList();
+            //return View();
+           return View(db.Restaurants.ToList());
+        }
+
+        public ActionResult Menu()
+        {
+            ViewBag.Produits = db.Produits.ToList();
+            ViewBag.Menus = db.Menus.ToList();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
